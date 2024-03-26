c2221a944bd606868693841be2936c2f86a3ed34
marc <cvmx.corpuz@gmai.com>
2024-03-26 20:14:16 +0000

    finished model doc (2)

--- machine_learning/network/model_bully.py
+++ machine_learning/network/model_bully.py
@@ -13,6 +13,7 @@ from json import dump, load
 
 # CONSTANTS
 CHECKPOINT_PATH_TF = "checkpoint/checkpoint_files/"
+MODEL_PATH_TF = f"checkpoint/models/"
 
 
 # The Model Handler
@@ -78,6 +79,49 @@ class Model:
                                      save_best_only=True,
                                      monitor="accuracy")
     
+    # Loads the targeted checkpoint_file into the model
+    def load_model(self, file_name):
+        path_name = f"{CHECKPOINT_PATH_TF}{file_name}.ckpt"
+        self.model.load_weights(path_name).expect_partial()
+
+    # Logs the variables in a text file
+    def log_model_variables(self):
+        # Fetching current time and logging variables
+        current_time = str(datetime.now().strftime("%Y%m%d-%H%M%S"))
+        path_name = f"{MODEL_PATH_TF}{current_time}.json"
+
+        # The data needed to be written or saved
+        to_write = {
+            "hidden_layer_count": self.hidden_layer_count,
+            "layer_sizes": self.layer_sizes,
+            "output_neuron_count": self.output_neuron_count,
+            "hidden_activation_func": self.hidden_activation_func,
+            "output_activation_func": self.output_activation_func,
+            "metrics": self.metrics,
+            "learning_rate": self.learning_rate,
+        }
+
+        # Dumping (writing) the file
+        with open(path_name, 'w', encoding='utf-8') as file:
+            dump(to_write, file, ensure_ascii=False, indent=4)
+    
+    # Loads model variables
+    def load_model_variables(self, model_file):
+        path_name = f"{MODEL_PATH_TF}{model_file}.json"
+
+        # Opening JSON file
+        with open(path_name, 'r+', encoding='utf-8') as file:
+            consts = load(file)
+
+            # Updating network parameters
+            self.hidden_layer_count = consts["hidden_layer_count"]
+            self.layer_sizes = consts["layer_sizes"]
+            self.output_neuron_count = consts["output_neuron_count"]
+            self.hidden_activation_func = consts["hidden_activation_func"]
+            self.output_activation_func = consts["output_activation_func"]
+            self.metrics = consts["metrics"]
+            self.learning_rate = consts["learning_rate"]
+    
     # Trainig the model
     def train(self, features_train, features_validate, labels_train, labels_validate, epochs):
 
@@ -95,17 +139,15 @@ class Model:
 
 
 # Creating new model    
-model = Model(2, [1, 1], 19, 'relu', 'softmax')
-model.build_model()
+model = Model()
 
-# Setting learning rate
-model.learning_rate = 0.01
-model.metrics = ["accuracy"]
+# Loading model variables
+model.load_model_variables("20240113-131830")
 
 # Building and compiling
+model.build_model()
 model.build_optimiser()
 model.compile_model()
-model.build_checkpoint()
 
 # Generating a set of training data
 train_x = [[i] for i in range(0, 5)]
@@ -115,4 +157,3 @@ validate_y = [[i] for i in range(0, 2)]
 
 # Training model
 model.train(train_x, validate_x, train_y, validate_y, 2)
-
